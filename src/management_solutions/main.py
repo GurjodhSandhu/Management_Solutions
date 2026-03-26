83afcbc95784053637c00cca0aca75dcf5843ecd
Gurjodh Sandhu (300173178) <gurjodh.sandhu@student.ufv.ca>
2026-03-26 14:14:27 -0700

    Initial commit

---
+++ src/management_solutions/main.py
@@ -0,0 +1,6 @@
+from management_solutions.models.Truck import truck
+
+truck1 = truck("volvo","dbx10", 2001,59999)
+
+print(truck1.brand)
+print("test")
---
+++ src/management_solutions/models/Truck.py
@@ -0,0 +1,13 @@
+class truck:
+    def __init__(self, brand: str, make: str, year: int, mileage: int):
+            self.mileage = mileage
+            self.year = year
+            self.brand = brand
+            self.make = make
+
+
+
+
+
+
+    
