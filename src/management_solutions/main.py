1b76b1c200a04a6a091ceedf9871a5b468b40643
Gurjodh Sandhu (300173178) <gurjodh.sandhu@student.ufv.ca>
2026-03-27 16:45:53 -0700

    testing new truck input method and truck object creation method

--- src/management_solutions/main.py
+++ src/management_solutions/main.py
@@ -1,7 +1,14 @@
 from management_solutions.models.Truck import truck
 from management_solutions.utils.Truck_util import get_truck_input
+from management_solutions.utils.Truck_util import new_truck
 
 print("test")
-kwarg = get_truck_input()
-truck1 = truck(**kwarg)
+
+kwarg = get_truck_input() #dictionary with information on truck is created from user inputs
+truck1 = new_truck(kwarg) #method is used to create the new object
+
 print(truck1.year)
+
+
+
+input("again?:")
