81ff381e1889b9f8e41b3851b64239e690753495
Gurjodh Sandhu (300173178) <gurjodh.sandhu@student.ufv.ca>
2026-03-28 19:39:49 -0700

    Code Cleanup: removed unused custom exception classes that were replaced by a single TruckValidationError exception class

--- src/management_solutions/utils/exceptions.py
+++ src/management_solutions/utils/exceptions.py
@@ -2,14 +2,3 @@ class TruckValidationError(Exception):
     def __init__(self, errors_dict: dict):
         self.errors = errors_dict
         super().__init__("Validation failed")
-
-
-
-class InvalidVinError(Exception):
-    pass
-class InvalidYearError(Exception):
-    pass
-class InvalidMileageError(Exception):
-    pass
-class InvalidPlateError(Exception):
-    pass
