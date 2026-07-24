8ea58b44a116bd50f778bc6558090c7e8032fb50
Gurjodh Sandhu (300173178) <gurjodh.sandhu@student.ufv.ca>
2026-07-23 17:03:01 -0700

    Implemented pydantic class setup with type checking and basic input rules for driver_name and licensenumber

--- src/management_solutions/models/Driver.py
+++ src/management_solutions/models/Driver.py
@@ -1,11 +1,17 @@
 from management_solutions.utils.exceptions import ValidationError
-from pydantic import BaseModel, ValidationError
+from pydantic import BaseModel,Field, ValidationError
+from typing import Annotated
 
 class Driver(BaseModel):
     driver_id: int | None = None
     assigned_truck_id: int|None = None
-    driver_name: str|None = None
-    driver_licensenumber: str|None = None
+    driver_name: Annotated[str|None, Field(min_length=0, max_length=20)] = None
+    driver_licensenumber: Annotated[str|None, Field(min_length=4)] = None
 
 
-test1 = Driver(driver_id=1, assigned_truck_id=2, driver_name="", driver_licensenumber="123")
+try:
+    driving = Driver(driver_id=1,assigned_truck_id=1,
+                     driver_licensenumber="1234",driver_name="faksssssssssssssssssssssssssssssssssssssssse name")
+    print(driving)
+except ValidationError as e:
+    print(e)
