6163f230463281fd36c75f4f1fed67a4028bf675
Gurjodh Sandhu (300173178) <gurjodh.sandhu@student.ufv.ca>
2026-08-05 11:21:30 -0700

    [feat] added frontend view for driver model displaying all drivers

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("",views.index,name="home"),
     path("truck/",views.truck_view,name="truck"),
+    path("driver/", views.driver_view, name="driver"),
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
+from django.db.models import Model
 from django.shortcuts import render
-from core.models import Truck
+from core.models import Truck, Driver
 
 
 # Create your views here.
@@ -7,7 +8,10 @@ def index(request):
     return render(request, 'home.html',
     )
 
-
 def truck_view(request):
-    trucks = Truck.objects.all()
-    return render(request, "truck.html",{"trucks": trucks})
+    trucks = Truck.objects.all() #queryset (list) of truck objects
+    return render(request, "truck.html",{"trucks": trucks}) #sends truck objects in trucks
+
+def driver_view(request):
+    drivers = Driver.objects.all()
+    return render(request, "driver.html",{"drivers": drivers})
